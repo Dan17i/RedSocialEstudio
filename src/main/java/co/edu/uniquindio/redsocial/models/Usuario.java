46984917ca56654dde488d122d75f423b9f0c47c
Sebas-GTorres <sebastian.torresm@uqvirtual.edu.co>
2025-04-23 07:48:16 -0500

    GestorContenidos
    Creacion base de la clase GestorContenidos

--- src/main/java/co/edu/uniquindio/redsocial/models/Usuario.java
+++ src/main/java/co/edu/uniquindio/redsocial/models/Usuario.java
@@ -7,19 +7,60 @@ package co.edu.uniquindio.redsocial.models;
  * @since 2025-04-02
  */
 public class Usuario {
-    private String nombre;
-    private String email;
 
-    public Usuario(String nombre, String email) {
+    private String nombre;
+    private String id;
+    private ListaEnlazada<String> intereses;
+    private ListaEnlazada<Contenido> historialContenidos;
+    private ListaEnlazada<Valoracion> valoraciones;
+
+    public Usuario(String nombre, String id,
+                   ListaEnlazada<String> intereses, ListaEnlazada<Contenido> historialContenidos,
+                   ListaEnlazada<Valoracion> valoraciones) {
         this.nombre = nombre;
-        this.email = email;
+        this.id = id;
+        this.intereses = intereses;
+        this.historialContenidos = historialContenidos;
+        this.valoraciones = valoraciones;
     }
 
     public String getNombre() {
         return nombre;
     }
 
-    public String getEmail() {
-        return email;
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public ListaEnlazada<String> getIntereses() {
+        return intereses;
+    }
+
+    public void setIntereses(ListaEnlazada<String> intereses) {
+        this.intereses = intereses;
+    }
+
+    public ListaEnlazada<Contenido> getHistorialContenidos() {
+        return historialContenidos;
+    }
+
+    public void setHistorialContenidos(ListaEnlazada<Contenido> historialContenidos) {
+        this.historialContenidos = historialContenidos;
+    }
+
+    public ListaEnlazada<Valoracion> getValoraciones() {
+        return valoraciones;
+    }
+
+    public void setValoraciones(ListaEnlazada<Valoracion> valoraciones) {
+        this.valoraciones = valoraciones;
     }
 }

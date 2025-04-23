eaf4876b12579eb066db78ac7ea9c05780bd2640
Sebas-GTorres <sebastian.torresm@uqvirtual.edu.co>
2025-04-23 07:32:26 -0500

    Create ArbolBinarioBusqueda.java
    Creacion base de la clase ArbolbinarioBusqueda sin metodos completos

---
+++ src/main/java/co/edu/uniquindio/redsocial/models/ArbolBinarioBusqueda.java
@@ -0,0 +1,32 @@
+package co.edu.uniquindio.redsocial.models;
+
+public class ArbolBinarioBusqueda <T>{
+
+    NodoABB<T> raiz;
+
+    public ArbolBinarioBusqueda(NodoABB<T> raiz) {
+        this.raiz = raiz;
+    }
+
+    public NodoABB<T> getRaiz() {
+        return raiz;
+    }
+
+    public void setRaiz(NodoABB<T> raiz) {
+        this.raiz = raiz;
+    }
+
+    public  void insertar(String clave, T valor){
+
+    }
+
+    public ListaEnlazada<T> buscar(String clave){
+
+        return;
+    }
+
+    public ListaEnlazada<Contenido> listarContenidosPorTema(string tema){
+
+        
+    }
+}

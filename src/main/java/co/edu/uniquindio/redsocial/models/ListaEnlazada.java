b7b19dbf53db8523856c72f269ffcfab1ee13918
Sebas-GTorres <sebastian.torresm@uqvirtual.edu.co>
2025-04-23 09:25:34 -0500

    Feat: creacion parcia de clase ListaEnlazada

--- src/main/java/co/edu/uniquindio/redsocial/models/ListaEnlazada.java
+++ src/main/java/co/edu/uniquindio/redsocial/models/ListaEnlazada.java
@@ -1,4 +1,50 @@
 package co.edu.uniquindio.redsocial.models;
 
 public class ListaEnlazada<T> {
+
+    NodoLista<T> cabeza;
+    int tamanio;
+
+    public ListaEnlazada() {
+        cabeza = null;
+        tamanio = 0;
+    }
+
+    public void agregar(T elemento) {
+
+    }
+
+    public void eliminar(int posicion) {
+
+    }
+
+    public boolean buscar(T elemento) {
+        return false;
+    }
+
+    public T obtener(int posicion){
+
+        return null;
+    }
+
+    public boolean estaVacia() {
+
+        return cabeza == null;
+    }
+
+    public NodoLista<T> getCabeza() {
+        return cabeza;
+    }
+
+    public void setCabeza(NodoLista<T> cabeza) {
+        this.cabeza = cabeza;
+    }
+
+    public int getTamanio() {
+        return tamanio;
+    }
+
+    public void setTamanio(int tamanio) {
+        this.tamanio = tamanio;
+    }
 }
